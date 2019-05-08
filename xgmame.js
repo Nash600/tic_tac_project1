d8fac45df7a93e1393610f539c3acc6e1a9bdc84
Nash600 <nashat6000@gmail.com>
2019-05-08 10:58:41 -0400

    added reset game button

--- xgmame.js
+++ xgmame.js
@@ -4,14 +4,14 @@ let slots = [
     "","",""
 ];
 let player1= "x";
-let player2="o";
+let player2= "o";
 let turn = 1;
 let countMoves = 0;
 let gameOver = false;
 
 
 
-$('.flex-item').click(function(event){
+let board = function(event){
     if (gameOver === true ){
         $(this).off('click');
         console.log("no more playiong")
@@ -20,101 +20,139 @@ $('.flex-item').click(function(event){
         if (turn === 1) {
         
             event.target.innerHTML = player1;
-            const position =$(event.target).attr("data-j")
+            const position =$(event.target).attr("id")
             slots[position] = player1
             event.target.style.color = 'blue'
-            turn ++;
+            turn++;
             $(this).off('click');
         
         }else {
             event.target.innerHTML = player2;
-            const position =$(event.target).attr("data-j")
+            const position =$(event.target).attr("id")
             slots[position] = player2
             event.target.style.color = 'yellow'
-            turn --;
+            turn--;
             $(this).off('click');
         
         }
         console.log(slots);
         
-        if (countMoves === 9) {
-            console.log(' ties')
-        }
-        
        
+        
+        winning();
+        if (countMoves === 9 && gameOver !== true) {
+            setTimeout(function(){
+                alert("ties ")
+            },500);
+        }  
     }
-    if(winning()) {
-        let winner = turn === 1 ? console.log('player1 has won') : console.log("player 2 has won");
-        winner;
-    }
+    
      
+};
+
+
+$('#x').click(function () {
+        
+    player1 = 'x';
+    player2 ='o'
+    $('#x').removeClass('btn-x');
+    $('#o').addClass('btn-o');
+    
+
 })
+
+$('#o').click(function () {
+
+    player1 = 'o';
+    player2 ='x'
+    $('#o').removeClass('btn-o');
+    $('#x').addClass('btn-x');
+    
+})
+
+
+
+$('#reset').click(function(){
+    slots = [
+        "","","",
+        "","","",
+        "","",""
+    ];
+    countMoves=0;
+    gameOver=false;
+    $('.flex-item').text('')
+    $('.flex-item').on('click',board);
+    $('#x').addClass('btn-x');
+    $('#o').addClass('btn-o');
+})
+
+
+
+
+$('.flex-item').click(board)
 
 function winning() {
         //check virtical
     if (slots[0] !== "" && slots[0] === slots[1] && slots[0] === slots[2]){
-        console.log ('you won');
+      
         
         gameOver = true
     }
     else if (slots[3] !=='' && slots[3] === slots[4] && slots[3] === slots[5]) {
 
-        console.log ('you won');
+      
         
         gameOver = true
 
     }  else if (slots[6] !=='' && slots[6] === slots[7] && slots[6] === slots[8]) {
 
-        console.log ('you won');
+      
         gameOver = true
         //check horizontal
 
     } else if (slots[0] !=='' && slots[0] === slots[3] && slots[0] === slots[6]) {
 
-        console.log ('you won');
+      
         gameOver = true
 
     } else if (slots[1] !=='' && slots[1] === slots[4] && slots[1] === slots[7]) {
 
-        console.log ('you won');
+      
         gameOver = true
 
     } else if (slots[2] !=='' && slots[2] === slots[5] && slots[2] === slots[8]) {
 
-        console.log ('you won');
+      
         gameOver = true
         //check diagonal
     } else if (slots[0] !=='' && slots[0] === slots[4] && slots[0] === slots[8]) {
 
-        console.log ('you won');
+      
         gameOver = true
 
     } else if (slots[2] !=='' && slots[2] === slots[4] && slots[2] === slots[6]) {
 
-        console.log ('you won');
+      
         gameOver = true
+    }
+
+    if(gameOver === true) {
+        if (turn !==1){
+            setTimeout(function(){
+                alert("you won " + player1)
+            },500);
+        }else {
+            setTimeout(function(){
+                alert("you won " + player1)
+            },500);
+        }
+        
     }
 }
    
   
     
-$('#x').click(function () {
-        
-        player1 = 'o';
-        player2 ='x'
-        $('#x').removeClass('.btn-x');
-        $('#o').addClass('.btn-o');
-    
-    
-})
 
-$('#o').click(function () {
-  
-        player1 = 'x';
-        player2 ='o'
-        $('#o').removeClass('.btn-o');
-        $('#x').addClass('.btn-x');
-})
 
 
 
